489262cc5a32da44de218a75af983edfb0293c33
hzf <16118809+haze-finder@user.noreply.gitee.com>
2026-09-08 16:25:09 +0800

    add凌凌

--- lib/mockData.ts
+++ lib/mockData.ts
@@ -79,6 +79,16 @@ const initialUsers: User[] = [
     createdAt: '2024-01-01T00:00:00.000Z',
     updatedAt: '2024-01-01T00:00:00.000Z',
   },
+  {
+    id: '7',
+    username: 'lingling666',
+    password: 'lingling1314520',
+    birthday: '2011-07-17',
+    name: '凌凌宝宝',
+    avatar: '🌹',
+    createdAt: '2024-01-01T00:00:00.000Z',
+    updatedAt: '2024-01-01T00:00:00.000Z',
+  },
 ];
 
 // 计算initialUsers的指纹（用于检测代码修改）
